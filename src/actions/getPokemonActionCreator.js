364ef1a36bdc0451d952c1e3f4239737e5b6d612
Phillip Troutman <philliptroutman21@gmail.com>
2019-08-15 08:32:49 -0400

    connected async action creator to pokemon reducer

--- src/actions/getPokemonActionCreator.js
+++ src/actions/getPokemonActionCreator.js
@@ -1,6 +1,17 @@
-const getPokemonActionCreator = () => {
-  console.log(`getPokemonActionCreator has fired`);
-  return { foo: 'foo' };
-};
+import types from "./types";
+// This action creator returns a function
+// when it is dispatched from our pokemonButton component our redux-thunk middleware intercepts it
+// then it calls it with "dispatch" as an argument.
+// that function it just called must return a promise that will eventually be fulfilled.
+// when it is fulfilled you can use the dispatch method again to dispatch some object to the reducers.
+const getPokemonActionCreator = () => dispatch =>
+  fetch('https://pokeapi.co/api/v2/pokemon/ditto/')
+    .then(res => res.json())
+    .then(myJson => {
+      console.log('myJson: ', myJson);
+      console.log(`dispatch from pokemonAction: `, dispatch);
+      // dispatch the myJson value to the reducers
+      dispatch({ type: types.GET_POKEMON, payload: myJson });
+    });
 
 export default getPokemonActionCreator;
--- src/reducers/pokemonReducer.js
+++ src/reducers/pokemonReducer.js
@@ -4,11 +4,12 @@ const initialState = { pokemon: null };
 
 const pokemonReducer = (previousState = initialState, action) => {
   console.log(`previousState from PokemonReducer: `, previousState);
+  console.log(`action from PokemonReducer: `, action);
   switch (action.type) {
     case types.GET_POKEMON: {
       console.log(`made it to GET_POKEMON case`);
       // can't do a fetch call here because we need to return our piece of state
-      return 'foo';
+      return previousState;
     }
     default:
       return previousState;
@@ -16,4 +17,3 @@ const pokemonReducer = (previousState = initialState, action) => {
 };
 
 export default pokemonReducer;
- 
