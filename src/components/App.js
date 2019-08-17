0dabdee9e17e96dc4fcb9d1dfc57a92e8e07ab20
Phillip Troutman <philliptroutman21@gmail.com>
2019-08-17 13:31:48 -0400

    added functionality for updating document title

--- src/components/App.js
+++ src/components/App.js
@@ -6,17 +6,34 @@ import Decrement from './decrement';
 class App extends Component {
   constructor() {
     super();
-    this.state = { count: 0 };
+    this.state = {
+      count: 0,
+      countStatus: 'Neutral',
+    };
     this.decrementCount = this.decrementCount.bind(this);
     this.incrementCount = this.incrementCount.bind(this);
   }
 
+  componentDidMount() {
+    document.title = this.state.countStatus;
+  }
+
+  componentDidUpdate() {
+    document.title = this.state.countStatus;
+  }
+
   decrementCount() {
-    this.setState({ count: (this.state.count -= 1) });
+    this.setState({
+      count: (this.state.count -= 1),
+      countStatus: 'Going Down',
+    });
   }
 
   incrementCount() {
-    this.setState({ count: (this.state.count += 1) });
+    this.setState({
+      count: (this.state.count += 1),
+      countStatus: 'Going Up',
+    });
   }
 
   render() {
