03f620e0455706e4ae278345c4f1ffea13b54b99
Phillip Troutman <philliptroutman21@gmail.com>
2019-08-18 18:35:23 -0400

    adding logic for useState hook

--- README.md
+++ README.md
@@ -1,13 +1,1 @@
-# This is a simple app to experiment with new React Features
-## Checkout the various branches in this repo to see how different tools are used
-## Branch:
-- master
-  > Using vanilla React features
-- usingHooks
-  > Need to redo this with using react hooks
-- usingContextAPI
-  > need to redo this using the Context API
-- usingReduxThunk:
-  > Using redux thunk to query the pokemon API to obtain images of each pokemon
-
-> ### Note: If you add a new branch with new tech implementations, add the description to the README.md file in the `master` branch
+# Check the Master branch's README.md file for explanation on this repo
--- src/components/App.js
+++ src/components/App.js
@@ -1,53 +1,33 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Count from './count';
 import Increment from './increment';
 import Decrement from './decrement';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      count: 0,
-      countStatus: 'Neutral',
-    };
-    this.decrementCount = this.decrementCount.bind(this);
-    this.incrementCount = this.incrementCount.bind(this);
-  }
-
-  componentDidMount() {
-    document.title = this.state.countStatus;
-  }
-
-  componentDidUpdate() {
-    document.title = this.state.countStatus;
-  }
-
-  decrementCount() {
-    this.setState({
-      count: (this.state.count -= 1),
-      countStatus: 'Going Down',
-    });
-  }
-
-  incrementCount() {
-    this.setState({
-      count: (this.state.count += 1),
-      countStatus: 'Going Up',
-    });
-  }
-
-  render() {
-    return (
-      <div id="app">
-        <Count count={this.state.count} />
-        <hr />
-        <div id="button-container">
-          <Decrement decrementCount={this.decrementCount} />
-          <Increment incrementCount={this.incrementCount} />
-        </div>
+const App = props => {
+  // The useState hook returns an array. It's first element is or state value and it's second value is a function that can update that count. We are using array deconstruction to save those values in a variables count, and setCount.
+  const [count, setCount] = useState(0);
+  const [count]
+  useEffect(()=>{
+    document.
+  })
+  return (
+    <div id="app">
+      <Count count={count} />
+      <hr />
+      <div id="button-container">
+        <Decrement
+          decrementCount={() => {
+            setCount(count - 1);
+          }}
+        />
+        <Increment
+          incrementCount={() => {
+            setCount(count + 1);
+          }}
+        />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
