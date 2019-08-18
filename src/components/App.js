3d634928314206bf7bcae89e49d9803e6533c1dc
Phillip Troutman <philliptroutman21@gmail.com>
2019-08-18 18:38:30 -0400

    added useEffect hook to update the document.title propert

--- src/components/App.js
+++ src/components/App.js
@@ -6,10 +6,12 @@ import Decrement from './decrement';
 const App = props => {
   // The useState hook returns an array. It's first element is or state value and it's second value is a function that can update that count. We are using array deconstruction to save those values in a variables count, and setCount.
   const [count, setCount] = useState(0);
-  const [count]
-  useEffect(()=>{
-    document.
-  })
+  const [countStatus, setCountStatus] = useState('Neutral');
+
+  useEffect(() => {
+    document.title = countStatus;
+  });
+
   return (
     <div id="app">
       <Count count={count} />
@@ -18,11 +20,13 @@ const App = props => {
         <Decrement
           decrementCount={() => {
             setCount(count - 1);
+            setCountStatus('Going Down!');
           }}
         />
         <Increment
           incrementCount={() => {
             setCount(count + 1);
+            setCountStatus('Going Up!');
           }}
         />
       </div>
