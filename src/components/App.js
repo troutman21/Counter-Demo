6d7dd1b2e0dbf0449a6a4449a3baa08b69bdf976
Phillip Troutman <philliptroutman21@gmail.com>
2019-09-07 16:47:11 -0400

    removed console logs

--- src/components/App.js
+++ src/components/App.js
@@ -5,45 +5,11 @@ import Decrement from './decrement';
 import countReducer from '../reducers/countReducer';
 import changeCountActionCreator from '../action-creators/change-count-action-creator';
 
-// constructor() {
-//   super();
-//   this.state = {
-//     count: 0,
-//     countStatus: 'Neutral',
-//   };
-//   this.decrementCount = this.decrementCount.bind(this);
-//   this.incrementCount = this.incrementCount.bind(this);
-// }
-
-// componentDidMount() {
-//   document.title = this.state.countStatus;
-// }
-
-// componentDidUpdate() {
-//   document.title = this.state.countStatus;
-// }
-
-// decrementCount() {
-//   this.setState({
-//     count: (this.state.count -= 1),
-//     countStatus: 'Going Down',
-//   });
-// }
-
-// incrementCount() {
-//   this.setState({
-//     count: (this.state.count += 1),
-//     countStatus: 'Going Up',
-//   });
-// }
-
 const App = props => {
   const [state, dispatch] = useReducer(countReducer, {
     count: 0,
     counterStatus: 'Neutral 😐',
   });
-  console.log(`state: `, state);
-  console.log(`dispatch: `, dispatch);
 
   useEffect(() => {
     document.title = state.counterStatus;
