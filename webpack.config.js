eceed67578499b76b4f9d254c0a5bc7c263b748b
Phillip Troutman <philliptroutman21@gmail.com>
2019-09-07 16:00:04 -0400

    added html template plugin for better DX in development

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -20,6 +21,11 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
   devServer: {
     contentBase: './dist',
   },
